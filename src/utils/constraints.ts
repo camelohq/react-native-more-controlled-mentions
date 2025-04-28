c2efe1a1005b93df8323e85deb1d160a32785d40
Vu Pham <phamhoaivu911@gmail.com>
2025-04-28 15:21:42 +0700

    Update regex to make it compatible with react-mention

--- src/utils/constraints.ts
+++ src/utils/constraints.ts
@@ -1,19 +1,19 @@
 import { StyleProp, TextStyle } from 'react-native';
 
 /**
- * RegEx grouped results. Example - "{@}[Full Name](123abc)"
+ * RegEx grouped results. Example - "@[Full Name](123abc)"
  * We have 4 groups here:
- * - The whole original string - "{@}[Full Name](123abc)"
+ * - The whole original string - "@[Full Name](123abc)"
  * - Mention trigger - "@"
  * - Name - "Full Name"
  * - Id - "123abc"
  */
-const triggerRegEx = /({([^{^}]*)}\[([^[]*)]\(([^(^)]*)\))/i;
+const triggerRegEx = /(([^{^}]*)\[([^[]*)]\(([^(^)]*)\))/i;
 
 /**
  * We need this single group regex for using String.prototype.split method
  */
-const singleGroupTriggerRegEx = /({[^{^}]*}\[[^[]*]\([^(^)]*\))/gi;
+const singleGroupTriggerRegEx = /([^{^}]*\[[^[]*]\([^(^)]*\))/gi;
 
 const DEFAULT_ALLOWED_SPACES_COUNT = 1;
 
